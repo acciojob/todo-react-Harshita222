214bc019a59e476c8c9358e064bc5b1f1128f6b9
Harshita222 <kumariharsita501@gmail.com>
2024-06-30 19:54:32 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 
 import React from "react";
 import './../styles/App.css';
+import Todo from "./Todo";
 
 const App = () => {
   return (
     <div>
+        <Todo/>
         {/* Do not remove the main div */}
     </div>
   )
